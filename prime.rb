f8d1cd29f1eb6e094d95b4e4d3c2452916d3e629
Alex McAusland <alex.mcausland@gmail.com>
2012-11-12 15:05:37 +0000

    Adding main function.

--- prime.rb
+++ prime.rb
@@ -3,25 +3,33 @@ def calculateTableOfSquares(list)
     return list.map {|x| list.map {|y| x * y}}
 end
 
+# Returns the list of minimum column widths for table.
 def calculatePaddingForNumericTable(table)
     return table.transpose().map {|x| String(x.max).length}
 end
 
+# Prefixes each row in table with specified header.
 def insertRowHeaders(table, headers)
     return headers.zip(table).map {|header, row| ([header] << row).flatten}
 end
 
 def getFormatStringForPadding(rowPadding)
-    return rowPadding.map {|x| "%" + String(x) + "d"}.join(" ") + "\n"
+    return rowPadding.map {|x| "%" + String(x) + "s"}.join(" ")
 end
 
-def printNumericTable(list)
+def printTableOfSquares(list)
     table = insertRowHeaders(calculateTableOfSquares(list), list)
+    
     columnWidths = calculatePaddingForNumericTable(table)
-    headerRow = list.unshift(0 * columnWidths[0])
-    table = table.unshift(headerRow)
-    formatString = getFormatStringForPadding(columnWidths)
+    
+    # Add the header row, with a prefixed blank for alignment.
+    table = table.unshift(list.unshift(""))
+    formatString = getFormatStringForPadding(columnWidths) + "\n"
     
     table.each {|row| printf(formatString, *row) }
     return
 end
+
+# As requested, no library function in sight!
+firstTenPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+printTableOfSquares(firstTenPrimes)
--- testPrime.rb
+++ testPrime.rb
@@ -1,19 +1,17 @@
-# File:  tc_simple_number.rb
- 
-require "prime"
+ require "prime"
 require "test/unit"
  
 class TestPrime < Test::Unit::TestCase
  
-  def testEmptySquaresTable
-    assert_equal([], calculateTableOfSquares([]))
-  end
+    def testEmptySquaresTable
+        assert_equal([], calculateTableOfSquares([]))
+    end
     
-  def testSingleNumber
+    def testSquaresOfSingleElementTable
       assert_equal([[1]], calculateTableOfSquares([1]))
-  end
+    end
     
-    def test2X2
+    def testSquaresOf2X2Table
         assert_equal([[1, 2],
                       [2, 4]],
                      calculateTableOfSquares([1, 2]))
@@ -30,10 +28,10 @@ class TestPrime < Test::Unit::TestCase
     end
     
     def testConstructsFormatStringWithWidths
-        assert_equal("%1d %5d\n", getFormatStringForPadding([1, 5]))
+        assert_equal("%1d %5d", getFormatStringForPadding([1, 5]))
     end
     
     def testInsertsRowHeaders
-       assert_equal([[4, 1, 2]], insertRowHeaders([[1, 2]], [4])) 
+       assert_equal([[4, 1, 2]], insertRowHeaders([[1, 2]], [4]))
     end
 end
